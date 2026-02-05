e690051d736c9e2e33bdf34156b848c5ccd93eac
TediousTasks <TediousTasks@users.noreply.github.com>
2026-02-04 19:37:42 -0500

    fixe the /root/.local/share + fix Tor paths?

--- briar_mailbox/run.sh
+++ briar_mailbox/run.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/with-contenv bash
 set -e
 
-# Make Briar use Home Assistant's persistent data directory
+# Force all “user home” and XDG dirs into /data (persistent + writable in HA add-on)
 export HOME="/data"
 export XDG_DATA_HOME="/data/xdg/data"
 export XDG_CONFIG_HOME="/data/xdg/config"
 export XDG_CACHE_HOME="/data/xdg/cache"
 
-mkdir -p \
-  "$XDG_DATA_HOME" \
-  "$XDG_CONFIG_HOME" \
-  "$XDG_CACHE_HOME"
+mkdir -p "$XDG_DATA_HOME" "$XDG_CONFIG_HOME" "$XDG_CACHE_HOME"
 
-echo "Starting Briar Mailbox…"
 exec java -jar /app/briar-mailbox.jar
